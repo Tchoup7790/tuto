2b6eb649dadc54eb862d2d1971e043aa1bf61560
Baptiste <juliojuliobaptiste@gmail.com>
2024-01-24 13:21:34 +0100

    Date

---
+++ tuto_Javascript/date.js
@@ -0,0 +1,16 @@
+//date
+const d = new Date()
+console.log(d)
+console.log(d.getTimezoneOffset())
+console.log(d.toISOString())
+console.log(d.toUTCString())
+console.log(d.toLocaleDateString())
+
+const d1 = new Date(10)
+console.log(d1)
+
+const d2 = new Date(2023, 14, 42)
+console.log(d2)
+
+
+
