1dc47b2040d9a5b3f484b1df423fd24ac536d37b
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2024-04-26 09:52:42 +0200

    Enhance tuto_typescript

---
+++ tuto_Typescript/basics/src/app.ts
@@ -0,0 +1,48 @@
+const compteur = document.querySelector("#compteur") as HTMLButtonElement
+let i = 0
+
+const increment = (e : Event) =>{
+    e.preventDefault()
+    i++
+    compteur.querySelector('span').innerText = i.toString()
+}
+
+function printId(id :string | number){
+    if(typeof id === 'number'){
+        console.log((id *3).toString)
+    }else{
+        console.log(id.toUpperCase())
+    }
+}
+
+compteur.addEventListener('click', increment)
+
+
+type User = {firstname: string, lastname: string}
+
+const john : User = {firstname: "John", lastname: "Doe"}
+console.log(john)
+
+class A{
+    private a: string
+    public b = 4
+    protected c = 5
+    
+    constructor(aParam : string){
+        this.a = aParam
+    }
+
+    log() {
+        console.log(this.a)
+        console.log(this.b)
+        console.log(this.c)
+    }
+}
+
+interface Point {
+    x: number,
+    y: number
+}
+
+const aInstance = new A("hello world")
+aInstance.log();
