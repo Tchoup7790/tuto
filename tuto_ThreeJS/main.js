c9982f54e0b6d2444972db76b5770c777e35b22e
tchoup <julio_baptiste@icloud.com>
2024-05-01 19:25:09 +0200

    endding theejs

--- tuto_ThreeJS/main.js
+++ tuto_ThreeJS/main.js
@@ -3,9 +3,11 @@ import * as dat from 'dat.gui'
 import './style.css'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
+import img from './img/baniere.jpg'
+
 const render = new THREE.WebGLRenderer()
 
-render.shadowMap.enabled = true  
+render.shadowMap.enabled = true
 
 render.setSize(window.innerWidth, window.innerHeight)
 
@@ -13,12 +15,7 @@ document.querySelector('#app').appendChild(render.domElement)
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(
-    75, 
-    window.innerWidth / window.innerHeight,
-    .1,
-    1000
-)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 1000)
 
 const orbit = new OrbitControls(camera, render.domElement)
 
@@ -83,7 +80,49 @@ scene.add(sLightHelper)
 // scene.fog = new THREE.Fog('#FFFFFF', 0, 2000)
 scene.fog = new THREE.FogExp2('#FFFFFF', .01)
 
-render.setClearColor("#FF0000")
+// render.setClearColor("#FF0000")
+
+const textureLoader = new THREE.TextureLoader()
+scene.background = textureLoader.load(img)
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+// scene.background = cubeTextureLoader.load([
+//   img, img, img, img, img, img
+// ])
+
+const box2Geometry = new THREE.BoxGeometry(4, 4, 4)
+const box2Material = new THREE.MeshBasicMaterial({
+  // color: "#00FF00",
+  // map: textureLoader.load(img)
+})
+const box2MultiMaterial = [
+  new THREE.MeshBasicMaterial({map: textureLoader.load(img)}),
+  new THREE.MeshBasicMaterial({map: textureLoader.load(img)}),
+  new THREE.MeshBasicMaterial({color: "#00FF00"}),
+  new THREE.MeshBasicMaterial({map: textureLoader.load(img)}),
+  new THREE.MeshBasicMaterial({map: textureLoader.load(img)}),
+  new THREE.MeshBasicMaterial({color: "#00FFFF"}),
+]
+const box2 = new THREE.Mesh(box2Geometry, box2MultiMaterial)
+scene.add(box2)
+box2.position.set(0, 15, 10)
+// box2.material.map= textureLoader.load(img)
+
+
+const plane2Geometry = new THREE.PlaneGeometry(10, 10, 10, 10)
+const plane2Material = new THREE.MeshBasicMaterial({
+    color: "#FFFFFF",
+    wireframe: true
+})
+const plane2 = new THREE.Mesh(plane2Geometry, plane2Material)
+scene.add(plane2)
+plane2.position.set(10, 10, 15)
+
+plane2.geometry.attributes.position.array[0] -= 10 * Math.random()
+plane2.geometry.attributes.position.array[1] -= 10 * Math.random()
+plane2.geometry.attributes.position.array[2] -= 10 * Math.random()
+const lastPointZ = plane2.geometry.attributes.position.array.length -1
+plane2.geometry.attributes.position.array[lastPointZ] -= 10 * Math.random()
+
 
 const gui = new dat.GUI()
 
@@ -112,6 +151,17 @@ gui.add(options, 'intensify', 0, 100000)
 
 let step = 0
 
+const mousePosition = new THREE.Vector2()
+
+window.addEventListener('mousemove', (e) => {
+  mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1
+  mousePosition.y = (e.clientY / window.innerHeight) * 2 - 1
+})
+
+const rayCaster = new THREE.Raycaster()
+
+const sphereId = sphere.id
+box2.name = 'theBox'
 
 function animate(time){
     box.rotation.x = time/1000
@@ -125,8 +175,32 @@ function animate(time){
     spolight.penumbra = options.penumbra
     sLightHelper.update()
 
+    rayCaster.setFromCamera(mousePosition, camera)
+    const intersects = rayCaster.intersectObjects(scene.children)
+    // console.log(intersects)
+
+    for(let i = 0; i < intersects.length; i++){
+        if(intersects[i].object.id === sphereId)
+            intersects[i].object.material.color.set(0xFFFFFF)
+        if(intersects[i].object.name === "theBox"){
+            intersects[i].object.rotation.x = time/1000
+            intersects[i].object.rotation.y = time/1000
+        }
+    }
+
+    plane2.geometry.attributes.position.array[0] = 10 * Math.random()
+    plane2.geometry.attributes.position.array[1] = 10 * Math.random()
+    plane2.geometry.attributes.position.array[2] = 10 * Math.random()
+    plane2.geometry.attributes.position.array[lastPointZ] = 10 * Math.random()
+    plane2.geometry.attributes.position.needsUpdate = true
+
     render.render(scene, camera)
 }
 
 render.setAnimationLoop(animate)
 
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    render.setSize(window.innerWidth, window.innerHeight)
+} )
